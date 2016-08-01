ad17942143a9773574af3849f53888fbef296ee2
Rony Moura <ronymmoura@gmail.com>
2016-08-01 15:40:48 -0300

    Adding the project AreasDifferentProjects
    Adding a project that allows the MVC application to have areas separated in another project.

---
+++ C#/Templates/AreasDifferentProjects/AreasDifferentProjects/Areas/Area1/Area1AreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace AreasDifferentProjects
+{
+    public class Area1AreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Area1";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Area1_default",
+                "Area1/{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new string[] { "AreasDifferentProjects.Area1.Controllers" }
+            );
+        }
+    }
+}
